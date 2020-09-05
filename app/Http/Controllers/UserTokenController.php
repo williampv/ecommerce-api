a5b1e27a70b3c314760e0c108642b29ff673529a
William <williampv@gmail.com>
2020-09-05 18:16:27 -0500

    UserTokenController fix

--- app/Http/Controllers/UserTokenController.php
+++ app/Http/Controllers/UserTokenController.php
@@ -3,23 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserTokenController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $request->validate([
+        
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required',
             'device_name' => 'required'
         ]);
 
+        if ($validator->fails()) {
+            return $validator->messages();
+        }
+
         $user = User::where('email', $request->get('email'))->first();
 
-        if (!$user && !Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessage([
-                'email' => 'Email does not exist or match with our data'
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'email' => ['Email does not exist or match with our data']
             ]);
         }
 
